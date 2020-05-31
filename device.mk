357f0f4f281b88853bb15294839044f33b0e39cc
lightvortex <rishishukla242@gmail.com>
2020-05-31 18:57:28 +0530

    axon7:import biometrics from zte_tulip

--- device.mk
+++ device.mk
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.axon7
 
 # Media
 PRODUCT_COPY_FILES += \
