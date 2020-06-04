6c1188a05a70ec756549bce25093438ebae9d3d4
lightvortex <rishishukla242@gmail.com>
2020-06-04 14:12:10 +0530

    axon7:replace libbinder.so wih libbindergx.so

--- extract-files.sh
+++ extract-files.sh
@@ -61,8 +61,18 @@ function blob_fixup() {
     vendor/lib64/vendor.qti.gnss@1.0_vendor.so)
         patchelf --replace-needed "android.hardware.gnss@1.0.so" "android.hardware.gnss@1.0-v27.so" "${2}"
         ;;
-    vendor/lib64/hw/gffingerprint.default.so)    
-    patchelf --replace-needed "libsoftkeymaster.so" "libsoftkeymasterdevice.so" "${2}"
+    vendor/bin/gx_fpd)
+        patchelf --replace-needed "libbinder.so" "libbindergx.so" "${2}"
+        ;;
+    vendor/lib64/hw/fingerprint.msm8996.so)
+        patchelf --replace-needed "libbinder.so" "libbindergx.so" "${2}"
+        ;;
+    vendor/lib64/libfp_client.so)
+        patchelf --replace-needed "libbinder.so" "libbindergx.so" "${2}"
+        ;;
+    vendor/lib64/libfpservice.so)
+        patchelf --replace-needed "libbinder.so" "libbindergx.so" "${2}"
+        ;;
     esac
 }
 
